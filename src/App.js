ba9bc98d2c537d6c6375cc05d0fd0075e46267af
Anadil Mhr <anadilmhr858@gmail.com>
2024-04-02 12:23:01 -0700

    first change

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './component/Navbar';
+
+function Mine() {
+  return (
+    <>
+    <Navbar title= "Text"  about="About Us"/>
+    </>
+  )
+   
+    
+  
+}
+
+export default Mine;
+
+
+
+
